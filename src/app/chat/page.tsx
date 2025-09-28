448fc307f9d5116e9c7174eb94862108dc667e59
parkab27 <pkabaria77@gmail.com>
2025-09-28 08:20:28 -0400

    spacing out chat page better

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,12 +1,12 @@
-import ChatUI from '@/components/ChatUI';
 import AuthGuard from '@/components/AuthGuard';
+import ChatUI from '@/components/ChatUI';
 
 
 export default function ChatPage() {
   return (
     <AuthGuard>
     <div className="h-screen flex flex-col">
-      <div className="flex-1 max-w-4xl mx-auto w-full shadow-2xl bg-white border-2 border-gold-200">
+      <div className="flex-1 max-w-4xl mx-auto w-full shadow-2xl bg-white border-4 border-gold-200 mt-6 rounded-lg">
         <ChatUI />
       </div>
     </div>
